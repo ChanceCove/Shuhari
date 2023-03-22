14604353e70ad532855a0cc68fb2f0e6eeaea97b
ChanceCove <cccchancec@outlook.com>
2023-03-22 12:13:38 -0500

    working project

---
+++ app.js
@@ -0,0 +1,48 @@
+/* let age = 18;
+
+if (age < 21) {
+    console.log("you cannot enter this bar young boy");
+} else {
+if (age > 21)
+    console.log("come on in a have a drink, but drink resonsibly");
+}
+ */
+
+const plant = () => {
+    console.log("you are either a builder or a destroyer!")
+}
+
+plant();
+
+console.log(typeof {});
+console.log(3 * 20);
+
+let sleep = "midday"
+
+
+switch (sleep) {
+    case "morning":
+        console.log("still resting")
+    break;
+    case "midnight":
+        console.log("risk arguing with a narcissist")
+    break;
+    case "midday":
+        console.log("we out drinking that coffee away from this dude!")
+    break;
+
+
+
+}
+
+
+const frankl = () => {
+    console.log("he who has a why to live can deal with just about any how....")
+}
+frankl();
+
+
+let rice = "chance";
+let quote = `${rice} is a builder, not a destroyer anymore....`;
+
+console.log(quote);
